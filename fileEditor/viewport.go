7a9dcb41c0026fc5fbd16f1ffe7ffea42bc15f1a
Asiandayboy <russellcheeseday@gmail.com>
2024-06-18 14:37:24 -0400

    Add GetViewportWidth func

--- fileEditor/viewport.go
+++ fileEditor/viewport.go
@@ -3,3 +3,7 @@ package fileeditor
 func (f *FileEditor) GetViewportHeight() int {
 	return f.TermHeight - f.StatusBarHeight
 }
+
+func (f *FileEditor) GetViewportWidth() int {
+	return f.TermWidth - EditorLeftMargin + 1
+}

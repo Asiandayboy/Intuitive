2a9bb28146f3f9e4244cf7c0835b5cb3029cf06d
Asiandayboy <russellcheeseday@gmail.com>
2024-09-30 21:29:29 -0400

    Add comments to getters for viewport size

--- fileEditor/viewport.go
+++ fileEditor/viewport.go
@@ -1,9 +1,11 @@
 package fileeditor
 
+// The viewport height does not include the space occupied by the status bar
 func (f *FileEditor) GetViewportHeight() int {
 	return f.TermHeight - f.StatusBarHeight
 }
 
+// The viewport width does not include the margin space to the left
 func (f *FileEditor) GetViewportWidth() int {
 	return f.TermWidth - EditorLeftMargin + 1
 }
